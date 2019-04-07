7e7c62b34f4dbeac455b24183b738a44b0e69d23
Young Guo <young.guo@gmail.com>
2019-04-07 13:41:24 -0700

    Update string

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 import "./styles/App.css";
 
 export default class Calendar extends React.Component {
+  handleDateClick = arg => {
+    // TODO: GOOGLE CALENDAR STUFF
+    alert(arg.dateStr);
+  };
+
+  handleSelect = arg => {
+    // TODO: GOOGLE CALENDAR STUFF
+    alert(`selected + ${arg.startStr} to + ${arg.endStr}`);
+  };
+
   render() {
     return (
-      <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+      <FullCalendar
+        defaultView="timeGridWeek"
+        selectable
+        dateClick={this.handleDateClick}
+        select={this.handleSelect}
+        plugins={[interactionPlugin, dayGridPlugin, timeGridPlugin]}
+      />
     );
   }
 }
